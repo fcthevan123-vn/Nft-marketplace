a2eefc728789757d4b4dc537a7634005abe9f5ee
Lai The Van <fcthevan123@gmail.com>
2022-12-13 11:37:55 +0700

    13/12

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -23,3 +26,24 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
+
+// Dropdown sort
+const chainHeader = $$(".chain-header");
+const chainContent = $$(".chain-content");
+console.log(chainHeader);
+chainHeader.forEach((i, index) => {
+  i.addEventListener("click", () => {
+    chainContent[index].classList.toggle("chain-content-active");
+  });
+});
+
+// Click icon sort
+const containerNavLeft = $(".container-nav-left");
+const containerContent = $(".container-content");
+const contentSort = $(".content-sort");
+const contentItem = $(".content-item");
+containerNavLeft.onclick = function () {
+  containerContent.classList.toggle("content-active");
+  contentSort.classList.toggle("content-sort-active");
+  contentItem.classList.toggle("content-item-active");
+};
